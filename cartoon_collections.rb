4daec5e55f8effcc89ae5fea7b30d2498839b0cb
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-01 20:23:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(planateers)
   planateers.map {|person| "#{person.capitalize}!"}
 end
 
-def long_planeteer_calls(calls))
+def long_planeteer_calls(calls)
   calls.all? {|word| word.length > 4}
 end
 
