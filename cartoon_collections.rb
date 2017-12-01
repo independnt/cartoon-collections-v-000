cde6c0b3022529a6c01379285bfd7659d09882ca
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-01 20:19:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves (array)
 end
 
 def summon_captain_planet(planateers)
-  planateers.map {|person| "person!"}
+  planateers.map {|person| "person.capitalize!"}
 end
 
 def long_planeteer_calls# code an argument here
