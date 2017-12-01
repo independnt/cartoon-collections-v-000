32284156acdd0bc0964532982958414db317118b
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-01 20:38:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(snacks)
-  snacks.include?(cheese_types[0] || cheese_types[1] || cheese_types[2])
+  snacks.detect {|cheese| cheese_types.include?(cheese)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
